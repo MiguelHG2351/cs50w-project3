0d23c5edcfdab9c426150a8c6364ed9c27522724
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2022-06-03 20:26:32 -0600

    estilos de la home casi listos y productos agregados totalmente

--- orders/views.py
+++ orders/views.py
@@ -11,3 +11,15 @@ def index(request):
         'foods':foods,
         'popular_foods':popular_food
     })
+
+
+def orders_view(request):
+    popular_food = Popular_Food.objects.all()
+    foods = Food.objects.all()
+    # get data of foreign key
+    # a_popular_food = popular_food[0].food_id
+    # print(a_popular_food.food_name)
+    return render(request,'orders/order.html', {
+        'foods':foods,
+        'popular_foods':popular_food
+    })
